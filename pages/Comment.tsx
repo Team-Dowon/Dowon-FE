2070f038df3c0d0a01644b48fb0c7c4f2c27e091
ncb6206 <ncb6206@gmail.com>
2022-09-21 11:47:04 +0900

    댓글, 게시글, 요청 작성 api 작업중...

--- pages/Comment.tsx
+++ pages/Comment.tsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { StyleSheet, Text, View, SafeAreaView, FlatList } from "react-native";
 import { Input, Card } from "@rneui/themed";
 import { basic_theme } from "../theme";
 import PrimaryButton from "../component/PrimaryButton";
-import { axios_get } from "../api/api";
+import { axios_get, axios_post } from "../api/api";
+import UserContext from "../service/UserContext";
 
 type Commenttype = {
   user_nickname: string;
@@ -13,14 +14,29 @@ type Commenttype = {
 
 export default function Comment({ route }: any) {
   const [comment, setComment] = useState<string>("");
-
-  function CommentHandler() {
-    console.log("댓글 등록");
-  }
-
-  //댓글 가져오기 하는중 무시해도 됨
   const [ListComment, setListComment] = useState<Commenttype[]>([]);
+  const userContext = useContext(UserContext); // 전역변수 사용하기 위한 변수
 
+  const CommentHandler = async () => {
+    if (!userContext.userlogin) {
+      console.log("로그인 하셔야합니다!");
+    } else if (!comment) {
+      console.log("빈칸은 다 채워야함!");
+    } else {
+      axios_post(`post/${route.params.postid}/comment`, {
+        content: comment,
+      })
+        .then(async (response) => {
+          console.log(response.data); //comment 게시 완료
+        })
+        .catch(function (error) {
+          console.log(error);
+          console.log("댓글 게시 실패");
+        });
+    }
+  };
+
+  //댓글 가져오기
   const getListComment = async () => {
     axios_get(`post/${route.params.postid}/comment`)
       .then((response) => {
@@ -50,9 +66,19 @@ export default function Comment({ route }: any) {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Input style={styles.input} placeholder="댓글" onChangeText={setComment} value={comment} />
+      <Input
+        style={styles.input}
+        placeholder="댓글"
+        onChangeText={setComment}
+        value={comment}
+      />
       <PrimaryButton onPress={CommentHandler}>등록하기</PrimaryButton>
-      <FlatList style={styles.scroll} data={ListComment} renderItem={renderItem} keyExtractor={(item: Commenttype, index: number) => index.toString()} />
+      <FlatList
+        style={styles.scroll}
+        data={ListComment}
+        renderItem={renderItem}
+        keyExtractor={(item: Commenttype, index: number) => index.toString()}
+      />
     </SafeAreaView>
   );
 }
